9b8499367257befbeb0c4c015ef692f800d80dd2
mauricio2093 <mauricio2093@gmail.com>
2022-07-21 20:05:07 -0500

    Deployd Heroku

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 const routerApi = require('./routes');
 
 const app = express();
-const port = 3005;
+const port = process.env.PORT || 3005;
 
 app.use(express.json()); // middleware express
+
+const whitelist = ['http://127.0.0.1:5500'];
+
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hello! My server in express');
